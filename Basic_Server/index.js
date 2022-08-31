16ae7ecb5f4872b036d4674da46ec1d5b4b5e452
Shubham Athawane <67777638+shubhamathawane@users.noreply.github.com>
2022-08-31 11:16:04 +0530

    Add files via upload

--- Basic_Server/index.js
+++ Basic_Server/index.js
@@ -1,20 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoriesRoute = require("./routes/categories");
-const multer = require("multer");
-const cors = require("cors");
-const path = require("path");
-
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 port = process.env.PORT;
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const MongoUri = process.env.MONGO_URL;
 mongoose
@@ -24,26 +13,10 @@ mongoose
   .then(console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+app.listen(port, () => {
+    console.log(`Connected on Port ${port}`);
+})
 
-const upload = multer({ storage: storage });
-
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been Uploaded");
-});
-
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoriesRoute);
-
-app.listen(port, (req, res) => {
-  console.log(`Server is Running on Port ${port}`);
-});
+app.get('/', (req, res) => {
+    res.send("Hello all");
+})
