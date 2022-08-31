1c1c55402b7d1a15dd3eed358a5b924cfe6789a1
Shubham Athawane <67777638+shubhamathawane@users.noreply.github.com>
2022-08-31 11:14:10 +0530

    Create index.js

---
+++ Basic_Server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+port = process.env.PORT;
+
+// Importing Localhost mongodb url
+const MongoUri = process.env.MONGO_URL;
+mongoose
+  .connect(MongoUri, {
+     config: { autoIndex: true },
+    })
+  .then(console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Connected on Port ${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello all");
+})
